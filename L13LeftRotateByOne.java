98f661c96ed184c823f99c2ab600381a3f17d7c6
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-03-30 20:26:55 +0530

    initial.commit
    IT IS A JAVA ARRAY CODE TO APPLY LEFT ROTATE ELEMENT OF ARRAY BY ONE

---
+++ L13LeftRotateByOne.java
@@ -0,0 +1,24 @@
+public class L13LeftRotateByOne {
+    static void lRotateOne(int arr[],int n){
+        int temp=arr[0];
+        for(int i=1;i<n;i++){
+            arr[i-1]=arr[i];
+            
+        }
+        arr[n-1]=temp;
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        int n=5;
+        System.out.println("before left rotation");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        lRotateOne(arr,n);
+        System.out.println("before left rotation");
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
