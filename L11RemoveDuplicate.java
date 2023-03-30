e84306283108edd0a318f3ea79eee66ae8007256
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-03-30 20:23:33 +0530

    initial.commit
    IT IS A JAVA ARRAY CODE TO REMOVE DUPLICATION FROM ARRAY

---
+++ L11RemoveDuplicate.java
@@ -0,0 +1,34 @@
+public class L11RemoveDuplicate {
+    static int remDupls(int arr[],int n){
+        int res=1;
+        for(int i=1;i<n;i++){
+            if(arr[i]!=arr[res-1])
+            {
+                arr[res]=arr[i];
+                res++;
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+       int  arr[]={10,20,20,30,30,30};
+       int n=6;
+       System.out.println("Before Removal");
+
+       for(int i = 0; i < n; i++)
+       {
+       		System.out.print(arr[i]+" ");
+       }
+
+       System.out.println();
+
+       n = remDupls(arr, n);
+
+       System.out.println("After Removal");
+
+       for(int i = 0; i < n; i++)
+       {
+       		System.out.print(arr[i]+" ");
+       }
+    }
+}
