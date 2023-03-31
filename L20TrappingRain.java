a65e9532d0a9fe4cbba0f2f5252ae56cadd2fff2
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-03-31 11:45:37 +0530

    initial.commit
    IT IS A JAVA PROGRAM FOR TRAPPING RAIN PROBLEM

---
+++ L20TrappingRain.java
@@ -0,0 +1,26 @@
+public class L20TrappingRain {
+    static int getWater(int arr[]){
+        int n=arr.length;
+        int res=0;
+        int lmax[]=new int[n];
+        int rmax[]=new int[n];
+
+        lmax[0]=arr[0];
+        for(int i=1;i<n-1;i++){
+            lmax[i]=Math.max(lmax[i-1],arr[i]);
+        }
+
+        rmax[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            rmax[i]=Math.max(rmax[i+1] , arr[i]);
+        }
+        for(int i=1;i<n-1;i++){
+            res=res + (Math.min(lmax[i] , rmax[i]) - arr[i]);
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int arr[]={5,0,6,2,3};
+        System.out.println(getWater(arr));
+    }
+}
