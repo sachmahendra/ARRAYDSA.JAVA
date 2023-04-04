94d405ea4b8c37363e615726ba5091ec2bfdfdb2
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-04-04 16:28:27 +0530

    initial.commit
    IT IS A JAVA DSA CODE TO FIND OUT LONGEST EVEN ODD SUB ARRAY IN A GIVEN ARRAY

---
+++ L23LongestEvenOddSubArray.java
@@ -0,0 +1,20 @@
+public class L23LongestEvenOddSubArray {
+    static int maxEvenOdd(int arr[]){
+        int n= arr.length;
+        int res=1,curr=1;
+        for(int i=1;i<n;i++){
+            if((arr[i]%2==0 && arr[i-1]%2 !=0) || (arr[i]%2!=0 && arr[i-1]%2==0)){
+                curr++;
+                res=Math.max(res,curr);
+            }
+            else curr=1;
+        }
+        return res;
+
+    }
+    public static void main(String[] args) {
+        int arr[]={5,10,20,6,3,8};
+        int n=6;
+        System.out.println(maxEvenOdd(arr));
+    }
+}
