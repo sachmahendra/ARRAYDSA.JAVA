a6eea6e410677151c7b8522526d8d5ea8d446d4b
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-04-04 16:24:08 +0530

    initial.commit
    IT IS A JAVA DSA CODE TO FIND OUT MAXIMUM CONSUCATIVE ONE IN THE GIVEN ARRAY

---
+++ L21MaxConsucativeOne.java
@@ -0,0 +1,21 @@
+public class L21MaxConsucativeOne {
+    static int maxConsecutiveOne(int arr[]){
+        int res=0,curr =0;
+        int n = arr.length;
+        for(int i =0;i<n;i++){
+            if(arr[i]==0)
+            curr=0;
+            
+            else
+                curr++;
+                res=Math.max(res,curr);
+            
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int arr[] = {0, 1, 1, 0, 1, 1, 1}, n = 7;
+
+       System.out.println(maxConsecutiveOne(arr));
+    }
+}
